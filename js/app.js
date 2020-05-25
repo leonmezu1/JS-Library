e62464601f155944a0764ee36bec5ae1212e6e70
leonmezu1 <leo.mez@outlook.com>
2020-05-25 12:46:36 -0500

    Added library display method

--- js/app.js
+++ js/app.js
@@ -2,38 +2,57 @@
 let myLibrary = []
 
 const bForm = document.getElementById('bForm')
+const tableBody = document.getElementById('tableBody')
 
 // Add event
 
 bForm.addEventListener('submit', addBookToLibrary)
+document.addEventListener("DOMContentLoaded", renderLibrary(myLibrary))
 
 
 class Book {
- constructor (bTitle, bDescription, bNumber, bAuthor, bGenre) {
-   this.bTitle = bTitle
-   this.bDescription = bDescription
-   this.bNumber = bNumber
-   this.bAuthor = bAuthor
-   this.bGenre = bGenre
- }
+    constructor(bTitle, bDescription, bNumber, bAuthor, bGenre) {
+        this.bTitle = bTitle
+        this.bDescription = bDescription
+        this.bNumber = bNumber
+        this.bAuthor = bAuthor
+        this.bGenre = bGenre
+    }
 }
 
 // Submit book information
-function addBookToLibrary (e) {
-  e.preventDefault()
+function addBookToLibrary(e) {
+    e.preventDefault()
 
-  const bTitle = document.getElementById('bTitle').value
-  const bDescription = document.getElementById('bDescription').value
-  const bNumber = document.getElementById('bNumber').value
-  const bAuthor = document.getElementById('bAuthor').value
-  const bGenre = document.getElementById('bGenre').value
+    const bTitle = document.getElementById('bTitle').value
+    const bDescription = document.getElementById('bDescription').value
+    const bNumber = document.getElementById('bNumber').value
+    const bAuthor = document.getElementById('bAuthor').value
+    const bGenre = document.getElementById('bGenre').value
 
-  let book = new Book(bTitle, bDescription, bNumber, bAuthor, bGenre)
-  
-  myLibrary.push(book)  
+    let book = new Book(bTitle, bDescription, bNumber, bAuthor, bGenre)
+
+    myLibrary.push(book)
+    renderLibrary(myLibrary)
 }
 
-// Render the submmit book to view
-function renderBook () {
-  
+// Render the submit book to view
+function renderLibrary(myLibrary) {
+    tableBody.innerHTML = ''
+    if (myLibrary.length != 0) {
+        myLibrary.forEach((function (book, index) {
+            template =
+                `
+            <tr>
+                <th scope="row">${index + 1}</th>
+                <td>${book.bTitle}</td>
+                <td>${book.bDescription}</td>
+                <td>${book.bNumber}</td>
+                <td>${book.bAuthor}</td>
+                <td>${book.bGenre}</td>
+            </tr>
+            `
+            tableBody.innerHTML += template
+        }))
+    }
 }
