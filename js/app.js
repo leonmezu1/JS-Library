9efec74ecc368fcc04010492a5a22700b49676bf
Raymond Oluoch <rasholuoch@gmail.com>
2020-05-25 20:08:09 +0300

    adding submit book function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 // variables
+let myLibrary = []
 
-console.log('test')
+const bForm = document.getElementById('bForm')
+
+// Add event
+
+bForm.addEventListener('submit', addBookToLibrary)
+
+
+class Book {
+ constructor (bTitle, bDescription, bNumber, bAuthor, bGenre) {
+   this.bTitle = bTitle
+   this.bDescription = bDescription
+   this.bNumber = bNumber
+   this.bAuthor = bAuthor
+   this.bGenre = bGenre
+ }
+}
+
+// Submit book information
+function addBookToLibrary (e) {
+  e.preventDefault()
+
+  const bTitle = document.getElementById('bTitle').value
+  const bDescription = document.getElementById('bDescription').value
+  const bNumber = document.getElementById('bNumber').value
+  const bAuthor = document.getElementById('bAuthor').value
+  const bGenre = document.getElementById('bGenre').value
+
+  let book = new Book(bTitle, bDescription, bNumber, bAuthor, bGenre)
+  
+  myLibrary.push(book)  
+}
+
+// Render the submmit book to view
+function renderBook () {
+  
+}
